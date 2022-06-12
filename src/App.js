c9b728150548701c660f46da5b9cc50386464c1a
Lewis Broadhurst <lewis1broadhurst@gmail.com>
2022-06-12 11:19:55 +0100

    added counter

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
+import { useState } from 'react';
+
 import './stylesheets/App.css';
-import Person from './components/Person.js'
+import Person from './components/Person.js';
 
 const App = () => {
 
   // const name = "Lewis";
   // const isNameShowing = true;
 
+  const [counter, setCounter] = useState(0);
+
   return (
     <div className="App">
       <Person name={"Lewis"} lastName={"Broadhurst"} age={27} />
       <Person name={"Nathan"} />
       <Person name={"Sharon"} />
+
+      <button onClick={() => {setCounter(counter.valueOf() - 1)}}>-</button>
+      <h1>{counter}</h1>
+      <button onClick={() => {setCounter(counter.valueOf() + 1)}}>+</button>
     </div>
   );
 }
